8cc831a7b87c69a22dc345d6cebe2a9d7cc487e5
Miguel Anson <71834827+miganson@users.noreply.github.com>
2023-07-29 17:08:16 +0800

    added css to Home

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -1,35 +1,53 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-import './Home.css';  // import the CSS
+import "./Home.css";
 import { HomeProps } from "../../types/quizInterfaces";
-
-
 
 export const Home: React.FC<HomeProps> = ({ data }) => {
   const navigate = useNavigate();
 
   const handleButtonClick = (activityOrder: number) => {
     if (data) {
-      const activity = data.activities.find(activity => activity.order === activityOrder);
+      const activity = data.activities.find(
+        (activity) => activity.order === activityOrder
+      );
       if (activity) navigate("/quiz/" + activity.order);
     }
-  }
+  };
+
+  const handleResultsClick = () => {
+    navigate("/score");
+  };
 
   return (
     <div className="home-container">
-      <h1>Error Find</h1>
-      {[1, 2, 3, 4, 5].map(order => {
-        const activity = data?.activities.find(activity => activity.order === order);
+      <div className="line">
+        <div className="text-container">
+          <h3 className="title">CAE</h3>
+          <h1 className="title">Error Find</h1>
+        </div>
+      </div>
+      {[1, 2, 3, 4, 5].map((order) => {
+        const activity = data?.activities.find(
+          (activity) => activity.order === order
+        );
         return (
-          <div 
+          <div
             key={order}
+            className="line"
             onClick={() => handleButtonClick(order)}
-            className={activity ? 'text-blue' : 'text-gray'}
           >
-            {activity ? activity.activity_name : `Quiz ${order}`}
+            <div className="text-container">
+              <div className={activity ? "text-blue" : "text-gray"}>
+                {activity ? activity.activity_name : `Quiz ${order}`}
+              </div>
+            </div>
           </div>
-        )
+        );
       })}
+      <div className="text-container" onClick={handleResultsClick}>
+        <h3 className="results">RESULTS</h3>
+      </div>
     </div>
   );
 };
